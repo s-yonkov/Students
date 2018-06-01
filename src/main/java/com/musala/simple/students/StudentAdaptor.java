8aa48dfb48f7927a70d34d3471b38bb9461bf1cd
Stefan Yonkov <stefan.yonkov@musala.com>
2018-06-01 18:22:45 +0300

    Demo of the students with MongoDB instead of local JSON file. Not finalized(in progress)

---
+++ src/main/java/com/musala/simple/students/StudentAdaptor.java
@@ -0,0 +1,20 @@
+package com.musala.simple.students;
+
+import com.mongodb.BasicDBObject;
+import com.mongodb.DBObject;
+
+public class StudentAdaptor {
+
+	public static final DBObject toDBObject(Student student) {
+		return new BasicDBObject("_id", student.getId()).append("name", student.getName())
+				.append("age", student.getAge()).append("grade", student.getGrade());
+	}
+
+	public static final Student toStudentObject(DBObject dbObject) {
+
+		dbObject.toMap();
+
+		return null;
+	}
+
+}
