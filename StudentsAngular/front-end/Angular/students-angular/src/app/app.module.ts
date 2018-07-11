a7ee0e09632f7aae0afdb637a5828fb6297327df
Stefan Yonkov <stefan.yonkov@musala.com>
2018-07-11 13:26:39 +0300

    Adding and integrating bootstrap to the project.

--- StudentsAngular/front-end/Angular/students-angular/src/app/app.module.ts
+++ StudentsAngular/front-end/Angular/students-angular/src/app/app.module.ts
@@ -2,7 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
-
+import { AlertModule } from 'ngx-bootstrap';
 import { AppComponent } from './app.component';
 import { GetAllStudentsComponent } from './components/getAll/getAll.component';
 
@@ -14,7 +14,8 @@ import { GetAllStudentsComponent } from './components/getAll/getAll.component';
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    AlertModule.forRoot()
   ],
   providers: [],
   bootstrap: [AppComponent]
